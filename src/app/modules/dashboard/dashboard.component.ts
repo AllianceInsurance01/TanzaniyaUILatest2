c1d53647163ddb38afb3341f3a27fd3196499db2
MaanWork <maansarovarwork@gmail.com>
2023-08-10 10:33:55 +0530

    Dashboard Design Changes & Navbar toggle Issue Fixed

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -17,12 +17,25 @@ export class DashboardComponent implements OnInit {
 
   public datasets: any;
   public data: any;
-  public salesChart;
+  public salesChart;tabSection:any='policy';
   public clicked: boolean = true;
   public clicked1: boolean = false;
-
+  referralList:any[]=[];customerList:any[]=[];
   ngOnInit() {
-
+    this.customerList = [
+      {"CustomerName":"SteveSmith","Type":"Individual"},
+      {"CustomerName":"Steve James","Type":"Individual"},
+      {"CustomerName":"Michael","Type":"Individual"},
+      {"CustomerName":"Thomas","Type":"Individual"},
+      {"CustomerName":"Peter England","Type":"Individual"},
+    ]
+    this.referralList = [
+      {"CustomerName":"SteveSmith","QuoteNo":"Q03622","ReferenceNumber":"MOT-05450","Remarks":"Test"},
+      {"CustomerName":"SteveJames","QuoteNo":"Q03623","ReferenceNumber":"MOT-05451","Remarks":"Test"},
+      {"CustomerName":"SteveSmith","QuoteNo":"Q03624","ReferenceNumber":"MOT-05452","Remarks":"Test"},
+      {"CustomerName":"SteveJames","QuoteNo":"Q03625","ReferenceNumber":"MOT-05453","Remarks":"Test"},
+      {"CustomerName":"SteveJames","QuoteNo":"Q03626","ReferenceNumber":"MOT-05454","Remarks":"Test"},
+    ]
     this.datasets = [
       [0, 20, 10, 30, 15, 40, 20, 60, 60],
       [0, 20, 5, 25, 10, 30, 15, 40, 40]
@@ -49,7 +62,7 @@ export class DashboardComponent implements OnInit {
 			data: chartExample1.data
 		});
   }
-
+  setTabSection(name){this.tabSection=name;}
 
   public updateOptions() {
     this.salesChart.data.datasets[0].data = this.data;
